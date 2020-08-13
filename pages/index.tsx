30fcc8857fc258d6a83409ed4e49acd840dd86c6
ivso0001 <ivso0001@naver.com>
2020-08-13 13:23:06 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default () => {
+  return (
+    <div className="container">
+      <Head>
+        <title>RNTBase</title>
+      </Head>
+      <main>
+        <h1 className="title">
+          Rect Next Typescript
+        </h1>
+      </main>
+    </div>
+  )
+}
